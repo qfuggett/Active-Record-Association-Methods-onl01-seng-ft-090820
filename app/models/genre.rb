51bd3f1857ecb41b168fa4120aa090e97f5fbce2
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-12 17:21:45 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    self.artists.map |n| do
+    self.artists.map do |n|
       n.name
     end
     # return an array of strings containing every musician's name
