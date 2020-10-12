8cfee5f61ea7eeffbf337a158a1446b3eb639113
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-12 17:19:05 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,17 +4,14 @@ class Artist < ActiveRecord::Base
   
 
   def get_genre_of_first_song
-    self.songs.first.genres
     #return the genre of the artist's first saved song
   end
 
   def song_count
-    self.songs.count
     #return the number of songs associated with the artist
   end
 
   def genre_count
-    self.genres.count
     #return the number of genres associated with the artist
   end
 end
